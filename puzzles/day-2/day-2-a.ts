ed2e7087ed390915942696398813b460727e4299
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-02 18:37:12 +0000

    feat: 🎸 add day 2, parts A and B

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,65 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const maxValues = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+// Example line:
+// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const games = data.map((line) => {
+    if (line.length === 0)
+      return { gameID: 0, maxRed: 0, maxBlue: 0, maxGreen: 0 };
+
+    const colourValues = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+
+    const [game, rest] = line.split(':');
+
+    const revealItems = rest.split(';').flatMap((reveal) => reveal.split(','));
+
+    revealItems.forEach((item) => {
+      const numberRegex = /(\d+)/;
+      const colourRegex = /(green|blue|red)/;
+
+      const number = Number(item.match(numberRegex)[0]);
+      const [colour] = item.match(colourRegex);
+
+      const currentMax = colourValues[colour];
+
+      if (currentMax < number) {
+        colourValues[colour] = number;
+      }
+    });
+
+    const { red, green, blue } = colourValues;
+
+    return {
+      gameID: parseInt(game.substring(5)),
+      maxRed: red,
+      maxGreen: green,
+      maxBlue: blue,
+    };
+  });
+
+  const possibleGames = games.filter((game) => {
+    return !(
+      game.maxBlue > maxValues.blue ||
+      game.maxRed > maxValues.red ||
+      game.maxGreen > maxValues.green
+    );
+  });
+
+  return possibleGames.reduce((prev, curr) => prev + curr.gameID, 0);
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -1,9 +1,48 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+// Example line:
+// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  return data
+    .map((line) => {
+      if (line.length === 0) return 0;
+
+      const colourValues = {
+        red: 0,
+        green: 0,
+        blue: 0,
+      };
+
+      const revealItems = line
+        .split(':')[1]
+        .split(';')
+        .flatMap((reveal) => reveal.split(','));
+
+      revealItems.forEach((item) => {
+        const numberRegex = /(\d+)/;
+        const colourRegex = /(green|blue|red)/;
+
+        const number = Number(item.match(numberRegex)[0]);
+        const [colour] = item.match(colourRegex);
+
+        const currentMax = colourValues[colour];
+        if (currentMax === undefined)
+          throw new Error('OOPSIE DOOPSIE SOMETHING WENT WRONG: ' + colour);
+
+        if (currentMax < number) {
+          colourValues[colour] = number;
+        }
+      });
+
+      const { red, green, blue } = colourValues;
+
+      return red * green * blue;
+    })
+    .reduce((prev, curr) => prev + curr, 0);
 }
 
 const answer = await day2b();
