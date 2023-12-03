284003c7542d0b59761f01f6d7856e98f9275612
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-03 21:24:03 +0000

    feat: 🎸 add day 3, part A and B

--- puzzles/day-3/day-3-a.ts
+++ puzzles/day-3/day-3-a.ts
@@ -1,9 +1,45 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const specialCharacterRegex = /[^A-Za-z 0-9.]/;
+
 export async function day3a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let sum = 0;
+  for (let i = 0; i < data.length; i++) {
+    const line = data[i];
+    const matches = line.matchAll(/(\d+)/g);
+
+    for (const match of matches) {
+      const number = match[0];
+      const startIndex = match.index;
+      const endIndex = startIndex + number.length;
+      const substringStartIndex = startIndex > 0 ? startIndex - 1 : startIndex;
+      const substringEndIndex =
+        endIndex < line.length ? endIndex + 1 : endIndex;
+
+      let lineToMatch = '';
+
+      if (i > 0) {
+        lineToMatch += data[i - 1].substring(
+          substringStartIndex,
+          substringEndIndex,
+        );
+      }
+      lineToMatch += line.substring(substringStartIndex, substringEndIndex);
+      if (i < data.length - 1) {
+        lineToMatch += data[i + 1].substring(
+          substringStartIndex,
+          substringEndIndex,
+        );
+      }
+      const specialCharsMatch = lineToMatch.match(specialCharacterRegex);
+      if (specialCharsMatch !== null) sum += Number(number);
+    }
+  }
+
+  return sum;
 }
 
 const answer = await day3a();
--- puzzles/day-3/day-3-b.ts
+++ puzzles/day-3/day-3-b.ts
@@ -1,9 +1,54 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+interface AdjacentNumber {
+  start: number;
+  end: number;
+  number: number;
+}
+
 export async function day3b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let sum = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    const line = data[i];
+    const adjacentNumbers: AdjacentNumber[][] = [];
+    for (const match of line.matchAll(/\*/g)) {
+      const starIndex = match.index;
+      const numberRegex = /\d+/g;
+
+      const numbersInLine = Array.from(
+        [
+          ...line.matchAll(numberRegex),
+          ...(i > 0 ? data[i - 1].matchAll(numberRegex) : []),
+          ...(i < data.length - 1 ? data[i + 1].matchAll(numberRegex) : []),
+        ],
+        (match) => {
+          return {
+            start: match.index,
+            end: match.index + match[0].length,
+            number: Number(match[0]),
+          };
+        },
+      ).filter(
+        (number) =>
+          number.start === starIndex + 1 ||
+          number.end === starIndex ||
+          ((number.start < starIndex || number.start === starIndex) &&
+            (number.end > starIndex || number.end === starIndex)),
+      );
+
+      adjacentNumbers.push(numbersInLine);
+    }
+
+    sum += adjacentNumbers
+      .filter((numberArray) => numberArray.length === 2)
+      .map((numberArray) => numberArray[0].number * numberArray[1].number)
+      .reduce((prev, curr) => prev + curr, 0);
+  }
+
+  return sum;
 }
 
 const answer = await day3b();
