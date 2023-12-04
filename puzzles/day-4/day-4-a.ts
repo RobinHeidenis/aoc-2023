0518438d372141d8b930ffec95b7ac19b98fef2c
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-04 22:16:14 +0000

    feat: 🎸 add day 4, part A and B

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -3,7 +3,28 @@ import chalk from 'chalk';
 
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  return data
+    .map((line) => {
+      const [winningNumbersString, receivedNumbersString] = line
+        .split(': ')[1]
+        .split(' | ');
+      const winningNumbers = Array.from(
+        winningNumbersString.matchAll(/\d+/g),
+        (item) => Number(item[0]),
+      );
+      const receivedNumbers = Array.from(
+        receivedNumbersString.matchAll(/\d+/g),
+        (item) => Number(item[0]),
+      );
+      const winningReceivedNumbers = receivedNumbers.filter((num) =>
+        winningNumbers.includes(num),
+      );
+
+      if (winningReceivedNumbers.length === 0) return 0;
+      return winningReceivedNumbers.reduce((prev) => prev * 2, 0.5);
+    })
+    .reduce((prev, curr) => prev + curr, 0);
 }
 
 const answer = await day4a();
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -3,8 +3,53 @@ import chalk from 'chalk';
 
 export async function day4b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const cardMap: Map<number, { line: string; amount: number }> = new Map();
+
+  data.forEach((line, i) => {
+    const [cardNumberString, rest] = line.split(': ');
+    const cardNumber = Number(cardNumberString.match(/\d+/)[0]);
+    if (!cardMap.has(cardNumber)) cardMap.set(cardNumber, { line, amount: 1 });
+
+    const [winningNumbersString, receivedNumbersString] = rest.split('|');
+    const winningNumbers = Array.from(
+      winningNumbersString.matchAll(/\d+/g),
+      (item) => Number(item[0]),
+    );
+    const receivedNumbers = Array.from(
+      receivedNumbersString.matchAll(/\d+/g),
+      (item) => Number(item[0]),
+    );
+
+    const winningReceivedNumbers = receivedNumbers.filter((num) =>
+      winningNumbers.includes(num),
+    );
+
+    const { amount: currentLineAmount } = cardMap.get(cardNumber);
+
+    winningReceivedNumbers.forEach((_, i) => {
+      const cardMapNumber = cardNumber + i + 1;
+      if (!cardMap.has(cardMapNumber)) {
+        cardMap.set(cardMapNumber, {
+          line: data[cardMapNumber],
+          amount: 1 + currentLineAmount,
+        });
+        return;
+      }
+      const cardMapItem = cardMap.get(cardMapNumber);
+      cardMap.set(cardNumber + i + 1, {
+        ...cardMapItem,
+        amount: cardMapItem.amount + currentLineAmount,
+      });
+    });
+  });
+
+  return Array.from(cardMap.values()).reduce(
+    (prev, curr) => prev + curr.amount,
+    0,
+  );
 }
 
+console.time('run');
 const answer = await day4b();
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+console.timeEnd('run');
