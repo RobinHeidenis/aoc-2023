978b225993ad4718ca0becad84372552d0ec30a1
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-08 17:49:11 +0000

    feat: 🎸 add day 8, part A and B
    this one sucked again. It cannot e bruteforced and i had no idea what
    LCM meant

--- puzzles/day-8/day-8-a.ts
+++ puzzles/day-8/day-8-a.ts
@@ -2,8 +2,28 @@ import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
 export async function day8a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const [instructionsString, _, ...nodes] = await readData(dataPath);
+  const instructions = instructionsString.split('');
+  const nodesMap = new Map(
+    nodes.map((item) => {
+      const [node, rest] = item.split(' = ');
+      const [left, right] = rest.replaceAll(/[)(]/g, '').split(', ');
+
+      return [node, { node, L: left, R: right }];
+    }),
+  );
+
+  let steps = 0;
+  let node = nodesMap.get('AAA');
+  while (node.node !== 'ZZZ') {
+    for (const instruction of instructions) {
+      if (node.node === 'ZZZ') break;
+      steps++;
+      node = nodesMap.get(node[instruction]);
+    }
+  }
+
+  return steps;
 }
 
 const answer = await day8a();
--- puzzles/day-8/day-8-b.ts
+++ puzzles/day-8/day-8-b.ts
@@ -1,10 +1,48 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const lcm = (arr: number[]) => {
+  const gcd = (x: number, y: number) => (!y ? x : gcd(y, x % y));
+  const _lcm = (x: number, y: number) => (x * y) / gcd(x, y);
+  return [...arr].reduce((a, b) => _lcm(a, b));
+};
+
 export async function day8b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const [instructionsString, _, ...nodes] = await readData(dataPath);
+  const nodesMap = new Map(
+    nodes.map((item) => {
+      const [node, rest] = item.split(' = ');
+      const [left, right] = rest.replaceAll(/[)(]/g, '').split(', ');
+
+      return [node, { node, L: left, R: right }];
+    }),
+  );
+
+  const nodeStateMap = new Map(
+    Array.from(nodesMap.keys())
+      .filter((n) => n.endsWith('A'))
+      .map((n) => [n, { node: nodesMap.get(n), steps: 0 }]),
+  );
+
+  const stepsArr = [];
+
+  for (let [_, { node }] of nodeStateMap) {
+    let currentNode = node;
+    let steps = 0;
+    while (!currentNode.node.endsWith('Z')) {
+      for (const instruction of instructionsString.split('')) {
+        if (currentNode.node.endsWith('Z')) break;
+        steps++;
+        currentNode = nodesMap.get(currentNode[instruction]);
+      }
+    }
+    stepsArr.push(steps);
+  }
+
+  return lcm(stepsArr);
 }
 
+console.time('run');
 const answer = await day8b();
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+console.timeEnd('run');
