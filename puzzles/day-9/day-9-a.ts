f8a22431d5b5c03cab213d4c4abaf888d7befa93
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-09 14:13:55 +0000

    feat: 🎸 add day 9, part A and B
    They're nearly identical. This one was really easy

--- puzzles/day-9/day-9-a.ts
+++ puzzles/day-9/day-9-a.ts
@@ -3,7 +3,29 @@ import chalk from 'chalk';
 
 export async function day9a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  return data
+    .map((line) => {
+      const levels: number[][] = [line.split(' ').map(Number)];
+
+      while (!levels.at(-1).every((i) => i === 0)) {
+        const sequence: number[] = [];
+        const level = levels.at(-1);
+        for (let i = 0; i < level.length - 1; i++) {
+          sequence.push(level[i + 1] - level[i]);
+        }
+        levels.push(sequence);
+      }
+
+      levels.at(-1).push(0);
+      for (let i = levels.length - 2; i >= 0; i--) {
+        const level = levels[i];
+        level.push(level.at(-1) + levels[i + 1].at(-1));
+      }
+
+      return levels.at(0).at(-1);
+    })
+    .reduce((prev, curr) => prev + curr);
 }
 
 const answer = await day9a();
--- puzzles/day-9/day-9-b.ts
+++ puzzles/day-9/day-9-b.ts
@@ -3,7 +3,29 @@ import chalk from 'chalk';
 
 export async function day9b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  return data
+    .map((line) => {
+      const levels: number[][] = [line.split(' ').map(Number)];
+
+      while (!levels.at(-1).every((i) => i === 0)) {
+        const sequence: number[] = [];
+        const level = levels.at(-1);
+        for (let i = 0; i < level.length - 1; i++) {
+          sequence.push(level[i + 1] - level[i]);
+        }
+        levels.push(sequence);
+      }
+
+      levels.at(-1).unshift(0);
+      for (let i = levels.length - 2; i >= 0; i--) {
+        const level = levels[i];
+        level.unshift(level.at(0) - levels[i + 1].at(0));
+      }
+
+      return levels.at(0).at(0);
+    })
+    .reduce((prev, curr) => prev + curr);
 }
 
 const answer = await day9b();
