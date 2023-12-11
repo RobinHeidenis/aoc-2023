2e340c69d5a9d474469a48c97b364e600a4477f0
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-11 21:07:03 +0000

    feat: 🎸 add day 11, part A and B
    This one was fun. It was not too hard, but sometime I was smooth brain
    and did some calculations at the wrong place. I had to check someone
    elses code to fix it, but it works and I'm satisfied

--- puzzles/day-11/day-11-a.ts
+++ puzzles/day-11/day-11-a.ts
@@ -3,7 +3,64 @@ import chalk from 'chalk';
 
 export async function day11a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const rowsToExpand = data
+    .map((line, i) => {
+      if (!line.includes('#')) return i;
+      return null;
+    })
+    .filter((i) => typeof i === 'number');
+
+  const colsToExpand = [];
+  for (let i = 0; i < data[0].length; i++) {
+    let starInThisRow = false;
+    for (let j = 0; j < data.length; j++) {
+      if (data[j][i] === '#') starInThisRow = true;
+    }
+    if (!starInThisRow) colsToExpand.push(i);
+  }
+
+  let expandedSky = [...data];
+
+  rowsToExpand.forEach((skyIndex, i) => {
+    expandedSky.splice(
+      skyIndex + i,
+      0,
+      Array.from({ length: data[0].length }).fill('.').join(''),
+    );
+  });
+
+  colsToExpand.forEach((skyIndex, i) => {
+    expandedSky.forEach((row, index) => {
+      expandedSky[index] = row
+        .split('')
+        .toSpliced(skyIndex + i, 0, '.')
+        .join('');
+    });
+  });
+
+  const galaxies: { x: number; y: number }[] = [];
+  expandedSky.forEach((line, i) => {
+    const matches = line.matchAll(/#/g);
+    galaxies.push(
+      ...Array.from(matches, (match) => {
+        return {
+          x: match.index,
+          y: i,
+        };
+      }),
+    );
+  });
+
+  const pathLengths: number[] = [];
+  galaxies.forEach((galaxy, index) => {
+    const { x, y } = galaxy;
+    galaxies.slice(index + 1).forEach((g) => {
+      pathLengths.push(Math.abs(g.x - x) + Math.abs(g.y - y));
+    });
+  });
+
+  return pathLengths.reduce((prev, curr) => prev + curr);
 }
 
 const answer = await day11a();
--- puzzles/day-11/day-11-b.ts
+++ puzzles/day-11/day-11-b.ts
@@ -1,9 +1,60 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const ONE_MILLION = 1_000_000;
+
 export async function day11b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const rowsToExpand = data
+    .map((line, i) => {
+      if (!line.includes('#')) return i;
+      return null;
+    })
+    .filter((i) => typeof i === 'number');
+
+  const colsToExpand = [];
+  for (let i = 0; i < data[0].length; i++) {
+    let galaxyInThisRow = false;
+    for (let j = 0; j < data.length; j++) {
+      if (data[j][i] === '#') galaxyInThisRow = true;
+    }
+    if (!galaxyInThisRow) colsToExpand.push(i);
+  }
+
+  const galaxies: { x: number; y: number }[] = [];
+  data.forEach((line, i) => {
+    const matches = line.matchAll(/#/g);
+
+    galaxies.push(
+      ...Array.from(matches, (match) => {
+        const x = match.index;
+        let amountOfColCrosses = 0;
+        colsToExpand.forEach((col) => {
+          if (col < x) amountOfColCrosses++;
+        });
+        let amountOfRowCrosses = 0;
+        rowsToExpand.forEach((row) => {
+          if (row < i) amountOfRowCrosses++;
+        });
+
+        return {
+          x: x + amountOfColCrosses * (ONE_MILLION - 1),
+          y: i + amountOfRowCrosses * (ONE_MILLION - 1),
+        };
+      }),
+    );
+  });
+
+  const pathLengths: number[] = [];
+  galaxies.forEach((galaxy, index) => {
+    const { x, y } = galaxy;
+    galaxies.slice(index + 1).forEach((g) => {
+      pathLengths.push(Math.abs(g.x - x) + Math.abs(g.y - y));
+    });
+  });
+
+  return pathLengths.reduce((prev, curr) => prev + curr);
 }
 
 const answer = await day11b();
