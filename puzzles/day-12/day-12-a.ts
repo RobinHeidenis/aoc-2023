571ccb5076d18952fb073de2d9ad4ee5830dda47
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-13 15:51:38 +0000

    feat: 🎸 add day 12, part A and B
    part A is a BOGOSORT kind of solution, we just fill all the ? in with a
    randomized value and see if it's correct. It does a million iterations
    to arrive at (hopefully) the right solution, and could still be off. I
    had to run it 10x just to make sure iI got the right value. T Part B
    would be impossible to brute force, so we'd have to come up with a
    better solution. I'm simply too dumb to do this though, and therefore
    stole someone elses solution. I forgot who it was but thank you to that
    person

--- puzzles/day-12/day-12-a.ts
+++ puzzles/day-12/day-12-a.ts
@@ -1,10 +1,57 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const generateRandomKnowns = (string: string) => {
+  const generateRandomNum = () => Math.floor(Math.random() * 10);
+  const workArr = string.slice().split('');
+  for (let i = 0; i < workArr.length; i++) {
+    if (workArr[i] === '?') {
+      const randomNum = generateRandomNum();
+      if (randomNum < 5) workArr[i] = '.';
+      else workArr[i] = '#';
+    }
+  }
+  return workArr.join('');
+};
+
+const checkCorrect = (candidate: string, check: string) => {
+  const brokenMatches = Array.from(candidate.matchAll(/#+/g), (m) => m[0]);
+  const checkItems = check.split(',').map((c) => Number(c));
+  if (brokenMatches.length !== checkItems.length) return false;
+  for (let i = 0; i < checkItems.length; i++) {
+    if (brokenMatches[i]?.length !== checkItems[i]) return false;
+  }
+  return true;
+};
+
 export async function day12a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  return data
+    .map((line, index) => {
+      const [conditionsWithUnknowns, differentlyFormattedConditions] =
+        line.split(' ');
+
+      const seenVariationsMap = new Set();
+
+      if (index % 50 === 0) console.log('Iteration:', index);
+      let amountOfCorrectOptions = 0;
+      for (let i = 0; i < 1_000_000; i++) {
+        const string = generateRandomKnowns(conditionsWithUnknowns);
+        if (seenVariationsMap.has(string)) continue;
+        seenVariationsMap.add(string);
+        if (checkCorrect(string, differentlyFormattedConditions)) {
+          amountOfCorrectOptions++;
+          // console.log('valid:', string);
+        }
+      }
+      // console.log(line, amountOfCorrectOptions);
+      return amountOfCorrectOptions;
+    })
+    .reduce((prev, curr) => prev + curr);
 }
 
+console.time('Run');
 const answer = await day12a();
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+console.timeEnd('Run');
--- puzzles/day-12/day-12-b.ts
+++ puzzles/day-12/day-12-b.ts
@@ -3,7 +3,49 @@ import chalk from 'chalk';
 
 export async function day12b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const cache = new Map<string, number>();
+  const _countWays = (row: string, ns: number[]): number => {
+    row = row.replace(/^\.+|\.+$/, '');
+    if (row === '') return ns.length ? 0 : 1;
+    if (!ns.length) return row.includes('#') ? 0 : 1;
+    const key = [row, ns].join(' ');
+    if (cache.has(key)) return cache.get(key)!;
+
+    let result = 0;
+    const damaged = row.match(/^#+(?=\.|$)/);
+    if (damaged) {
+      if (damaged[0].length === ns[0]) {
+        result += _countWays(row.slice(ns[0]), ns.slice(1));
+      }
+    } else if (row.includes('?')) {
+      const total = ns.reduce((a, b) => a + b);
+      result += _countWays(row.replace('?', '.'), ns);
+      if ((row.match(/#/g) || []).length < total) {
+        result += _countWays(row.replace('?', '#'), ns);
+      }
+    }
+    cache.set(key, result);
+    return result;
+  };
+
+  return data
+    .map((line) => {
+      const [conditionsWithUnknowns, differentlyFormattedConditions] =
+        line.split(' ');
+      const [
+        unfoldedConditionsWithUnknowns,
+        unfoldedDifferentlyFormattedConditions,
+      ] = [
+        [...Array(5)].fill(conditionsWithUnknowns).join('?'),
+        [...Array(5)].fill(differentlyFormattedConditions).join(','),
+      ];
+      return _countWays(
+        unfoldedConditionsWithUnknowns,
+        unfoldedDifferentlyFormattedConditions.split(',').map(Number),
+      );
+    })
+    .reduce((prev, curr) => prev + curr);
 }
 
 const answer = await day12b();
