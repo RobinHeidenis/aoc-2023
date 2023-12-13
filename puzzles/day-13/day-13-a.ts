6fb25d6c51e0533d19a215ec03f3e9c16aeac68f
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-13 21:42:01 +0000

    feat: 🎸 add day 13, part A and B
    This one was a little difficult, I was quite tired today so I spent a
    lot of time trying ot get it to work

--- puzzles/day-13/day-13-a.ts
+++ puzzles/day-13/day-13-a.ts
@@ -1,9 +1,74 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const format = (text: string) => {
+  let lines = text.split('\n');
+  let sections: string[][] = [];
+  let section: string[] = [];
+  for (let line of lines) {
+    if (!line) {
+      sections.push([...section]);
+      section = [];
+    } else {
+      section.push(line);
+    }
+  }
+  sections.push([...section]);
+  return sections;
+};
+
+const findHorizontalReflection = (section: string[]) => {
+  let cCount = section[0].length;
+  for (let col = 0; col < cCount - 1; col++) {
+    let valid = true;
+    let l = col;
+    let r = col + 1;
+    while (l >= 0 && r < cCount) {
+      let lCol = section.map((row) => row[l]).join('');
+      let rCol = section.map((row) => row[r]).join('');
+      if (lCol !== rCol) {
+        valid = false;
+        break;
+      }
+      l--;
+      r++;
+    }
+    if (valid) return col + 1;
+  }
+  return null;
+};
+
+const findVerticalReflection = (section: string[]) => {
+  for (let row = 0; row < section.length - 1; row++) {
+    let valid = true;
+    let l = row;
+    let r = row + 1;
+    while (l >= 0 && r < section.length) {
+      let lRow = section[l];
+      let rRow = section[r];
+      if (lRow !== rRow) {
+        valid = false;
+        break;
+      }
+      l--;
+      r++;
+    }
+    if (valid) return (row + 1) * 100;
+  }
+  return null;
+};
+
 export async function day13a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = await readData(dataPath, true);
+
+  const sections = format(data);
+  let sum = 0;
+  for (let section of sections) {
+    let horizontal = findHorizontalReflection(section);
+    let vertical = findVerticalReflection(section);
+    sum += horizontal ?? vertical ?? 0;
+  }
+  return sum;
 }
 
 const answer = await day13a();
--- puzzles/day-13/day-13-b.ts
+++ puzzles/day-13/day-13-b.ts
@@ -1,9 +1,78 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const format = (text: string) => {
+  let lines = text.split('\n');
+  let sections: string[][] = [];
+  let section: string[] = [];
+  for (let line of lines) {
+    if (!line) {
+      sections.push([...section]);
+      section = [];
+    } else {
+      section.push(line);
+    }
+  }
+  sections.push([...section]);
+  return sections;
+};
+
+const getDiff = (left: string, right: string) => {
+  let diff = 0;
+  let i = 0;
+  while (i < left.length) {
+    if (left[i] !== right[i]) diff++;
+    i++;
+  }
+  return diff;
+};
+
+const findHorizontalReflection = (section: string[]) => {
+  let cCount = section[0].length;
+  for (let col = 0; col < cCount - 1; col++) {
+    let l = col;
+    let r = col + 1;
+    let diff = 0;
+    while (l >= 0 && r < cCount) {
+      let lCol = section.map((row) => row[l]).join('');
+      let rCol = section.map((row) => row[r]).join('');
+      diff += getDiff(lCol, rCol);
+      l--;
+      r++;
+    }
+    if (diff === 1) return col + 1;
+  }
+  return null;
+};
+
+const findVerticalReflection = (section: string[]) => {
+  for (let row = 0; row < section.length - 1; row++) {
+    let diff = 0;
+    let l = row;
+    let r = row + 1;
+    while (l >= 0 && r < section.length) {
+      let lRow = section[l];
+      let rRow = section[r];
+      diff += getDiff(lRow, rRow);
+      l--;
+      r++;
+    }
+    if (diff === 1) return (row + 1) * 100;
+  }
+  return null;
+};
+
 export async function day13b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = await readData(dataPath, true);
+
+  const sections = format(data);
+  let sum = 0;
+  for (let section of sections) {
+    let horizontal = findHorizontalReflection(section);
+    let vertical = findVerticalReflection(section);
+    sum += horizontal ?? vertical ?? 0;
+  }
+  return sum;
 }
 
 const answer = await day13b();
