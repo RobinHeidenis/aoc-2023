cd2c7ebbf66ccc83ed3ab00908e06981afb6e3b1
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-14 19:05:53 +0000

    feat: 🎸 add day 14, part A and B
    Part B wants you to run rotations 1 billion times, for funsies. This
    would take a long long time. 2 million items run on my machine in about
    3 minutes, s so 1 billion items would take about 25 hours. Fortunately,
    since this is a loop, we can just run 1000 iterations an dstill get the
    right answer! Thanks to the people on reddit for discovering this. I
    probably could've figured it out myself but I'm just not not built like
    that. So Other than that, this day was pretty easy, I really enjoyed
    part 1. Hope you have an amazing day! <3

--- puzzles/day-14/day-14-a.ts
+++ puzzles/day-14/day-14-a.ts
@@ -1,9 +1,49 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const rotateArray = (array: string[][], counterClockwise?: boolean) => {
+  return counterClockwise
+    ? array[0].map((val, index) =>
+        array.map((row) => row[row.length - 1 - index]),
+      )
+    : array[0].map((val, index) => array.map((row) => row[index]).reverse());
+};
+
 export async function day14a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = (await readData(dataPath)).map((l) => l.split(''));
+  const rotatedArray = rotateArray(data, true);
+
+  rotatedArray.forEach((line, index) => {
+    let newLine = '';
+    const lineSegments = line.join('').split(/(#)/g);
+    lineSegments.forEach((segment) => {
+      if (segment === '#') {
+        newLine += '#';
+        return;
+      }
+      if (segment === '.') {
+        newLine += '.';
+        return;
+      }
+      const zeroMatches = Array.from(segment.matchAll(/O/g), (i) => i[0]);
+      zeroMatches.forEach(() => (newLine += 'O'));
+      if (zeroMatches.length !== segment.length) {
+        for (let i = 0; i < segment.length - zeroMatches.length; i++) {
+          newLine += '.';
+        }
+      }
+    });
+    rotatedArray[index] = newLine.split('');
+  });
+
+  return rotatedArray.reduce(
+    (prev, curr) =>
+      prev +
+      curr
+        .reverse()
+        .reduce((prev, curr, i) => (curr === 'O' ? prev + i + 1 : prev), 0),
+    0,
+  );
 }
 
 const answer = await day14a();
--- puzzles/day-14/day-14-b.ts
+++ puzzles/day-14/day-14-b.ts
@@ -1,10 +1,75 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const rotateArray = (array: string[][], counterClockwise?: boolean) => {
+  return counterClockwise
+    ? array[0].map((_val, index) =>
+        array.map((row) => row[row.length - 1 - index]),
+      )
+    : array[0].map((_val, index) => array.map((row) => row[index]).reverse());
+};
+
+const moveRocksLeft = (array: string[][]): string[][] => {
+  const matrix = [...array];
+
+  matrix.forEach((line, index) => {
+    let newLine = '';
+    const lineSegments = line.join('').split(/(#)/g);
+    lineSegments.forEach((segment) => {
+      if (segment === '#') {
+        newLine += '#';
+        return;
+      }
+      if (segment === '.') {
+        newLine += '.';
+        return;
+      }
+      const zeroMatches = Array.from(segment.matchAll(/O/g), (i) => i[0]);
+      zeroMatches.forEach(() => (newLine += 'O'));
+      if (zeroMatches.length !== segment.length) {
+        for (let i = 0; i < segment.length - zeroMatches.length; i++) {
+          newLine += '.';
+        }
+      }
+    });
+    matrix[index] = newLine.split('');
+  });
+
+  return matrix;
+};
+
+const rotateCycle = (array: string[][]) => {
+  let arr = [...array];
+
+  for (let i = 0; i < 4; i++) {
+    arr = moveRocksLeft(arr);
+    arr = rotateArray(arr);
+  }
+
+  return arr;
+};
+
 export async function day14b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = (await readData(dataPath)).map((l) => l.split(''));
+  let rotatedArray = rotateArray(data, true);
+
+  console.table(rotatedArray);
+
+  for (let i = 0; i < 1000; i++) {
+    rotatedArray = rotateCycle(rotatedArray);
+  }
+
+  return rotatedArray.reduce(
+    (prev, curr) =>
+      prev +
+      curr
+        .reverse()
+        .reduce((prev, curr, i) => (curr === 'O' ? prev + i + 1 : prev), 0),
+    0,
+  );
 }
 
+console.time('Run');
 const answer = await day14b();
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+console.timeEnd('Run');
