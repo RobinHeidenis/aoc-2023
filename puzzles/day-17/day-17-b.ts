271f4dbcddd3ee196d6f799caae3dc53baaa8102
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-18 20:20:20 +0000

    feat: 🎸 add day 17, part A and B
    Another really tough one. I had never done pathfinding before, so I had
    to get some help from a youtube video to solve this challenge. Thank you
    to @hyper-neutrino for the solution in Python. Dijkstra's algorithm may
    be great, but it's also really complicated when you've never heard of it
    before. Anyway, I hope you have an amazing day!

--- puzzles/day-17/day-17-b.ts
+++ puzzles/day-17/day-17-b.ts
@@ -1,8 +1,108 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type Node = {
+  heatLoss: number;
+  y: number;
+  x: number;
+  directionY: number;
+  directionX: number;
+  directionSteps: number;
+};
+
 export async function day17b(dataPath?: string) {
-  const data = await readData(dataPath);
+  const nodes = (await readData(dataPath)).map((l, i) =>
+    l.split('').map(Number),
+  );
+
+  const seen = new Set<string>();
+
+  const customPriorityComparator = (a: Node, b: Node) =>
+    b.heatLoss - a.heatLoss;
+
+  const pq: Node[] = [
+    {
+      heatLoss: 0,
+      y: 0,
+      x: 0,
+      directionY: 0,
+      directionX: 0,
+      directionSteps: 0,
+    },
+  ];
+
+  while (pq.length) {
+    const { heatLoss, y, x, directionY, directionX, directionSteps } = pq
+      .sort(customPriorityComparator)
+      .pop();
+
+    if (
+      y === nodes.length - 1 &&
+      x === nodes[0].length - 1 &&
+      directionSteps >= 4
+    ) {
+      return heatLoss;
+    }
+
+    const key = `${y}, ${x}, ${directionY}, ${directionX}, ${directionSteps}`;
+
+    if (seen.has(key)) continue;
+
+    seen.add(key);
+
+    if (directionSteps < 10 && (directionX !== 0 || directionY !== 0)) {
+      const nextY = y + directionY;
+      const nextX = x + directionX;
+      if (
+        nextX >= 0 &&
+        nextX < nodes[0].length &&
+        nextY >= 0 &&
+        nextY < nodes.length
+      ) {
+        pq.push({
+          heatLoss: heatLoss + nodes[nextY][nextX],
+          x: nextX,
+          y: nextY,
+          directionX,
+          directionY,
+          directionSteps: directionSteps + 1,
+        });
+      }
+    }
+
+    if (directionSteps >= 4 || (directionY === 0 && directionX === 0)) {
+      for (const [newDirectionX, newDirectionY] of [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0],
+      ]) {
+        if (
+          (newDirectionX !== directionX || newDirectionY !== directionY) &&
+          (newDirectionX !== -directionX || newDirectionY !== -directionY)
+        ) {
+          const nextY = y + newDirectionY;
+          const nextX = x + newDirectionX;
+          if (
+            nextX >= 0 &&
+            nextX < nodes[0].length &&
+            nextY >= 0 &&
+            nextY < nodes.length
+          ) {
+            pq.push({
+              heatLoss: heatLoss + nodes[nextY][nextX],
+              x: nextX,
+              y: nextY,
+              directionX: newDirectionX,
+              directionY: newDirectionY,
+              directionSteps: 1,
+            });
+          }
+        }
+      }
+    }
+  }
+
   return 0;
 }
 
