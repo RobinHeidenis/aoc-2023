b4c4a3934d34f565592fcc18e0231a713277dcea
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-20 01:07:59 +0000

    feat: 🎸 add day 18, part A and B
    This one was not that difficult, but I did learn about the shoelace
    formula. That was pretty interesting, getting that one to work. I also
    had lots of fun with CRLF and LF (LF wins always btw, microsoft pls
    fix). In CRLF my parsing didn't work, and so it would spit out the wrong
    answer. We love to see it. Anyway, it's all good now. I hope you had a
    great day, keep up the great work!

--- puzzles/day-18/day-18-a.ts
+++ puzzles/day-18/day-18-a.ts
@@ -1,9 +1,37 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const UP = 'U';
+const DOWN = 'D';
+const LEFT = 'L';
+const RIGHT = 'R';
+
+type Coordinate = { x: number; y: number };
+
+type Direction = typeof UP | typeof DOWN | typeof LEFT | typeof RIGHT;
+
 export async function day18a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let area = 0;
+  let perimeter = 0;
+  let pos: Coordinate = { x: 0, y: 0 };
+  for (const line of data) {
+    const [direction, amountStr] = line.split(' ') as [Direction, string];
+    const amount = Number(amountStr);
+    let newPos: Coordinate;
+
+    if (direction === UP) newPos = { x: pos.x + amount, y: pos.y };
+    else if (direction === DOWN) newPos = { x: pos.x - amount, y: pos.y };
+    else if (direction === LEFT) newPos = { x: pos.x, y: pos.y - amount };
+    else newPos = { x: pos.x, y: pos.y + amount };
+
+    area += pos.x * newPos.y - newPos.x * pos.y;
+    perimeter += Math.abs(newPos.x - pos.x) + Math.abs(newPos.y - pos.y);
+    pos = newPos;
+  }
+
+  return (Math.abs(area) + perimeter) / 2 + 1;
 }
 
 const answer = await day18a();
--- puzzles/day-18/day-18-b.ts
+++ puzzles/day-18/day-18-b.ts
@@ -1,9 +1,50 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const RIGHT = '0';
+const DOWN = '1';
+const LEFT = '2';
+const UP = '3';
+
+type Coordinate = { x: number; y: number };
+
+type Direction = typeof UP | typeof DOWN | typeof LEFT | typeof RIGHT;
+
 export async function day18b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let area = 0;
+  let perimeter = 0;
+  let pos: Coordinate = { x: 0, y: 0 };
+  for (const line of data) {
+    const [_direction, _amountStr, hexStr] = line.split(' ') as [
+      Direction,
+      string,
+      string,
+    ];
+    const hex = hexStr.replaceAll(/[#()]/g, '');
+    const amountStr = hex.substring(0, 5);
+    const direction = hex.slice(5);
+    const amount = parseInt(amountStr, 16);
+    let newPos: Coordinate;
+
+    console.log(amount, direction, typeof direction);
+
+    if (direction === UP) newPos = { x: pos.x + amount, y: pos.y };
+    else if (direction === DOWN) newPos = { x: pos.x - amount, y: pos.y };
+    else if (direction === LEFT) newPos = { x: pos.x, y: pos.y - amount };
+    else if (direction === RIGHT) newPos = { x: pos.x, y: pos.y + amount };
+    else
+      throw new Error(
+        "Please change the input format from CRLF to LF, then it'll work",
+      );
+
+    area += pos.x * newPos.y - newPos.x * pos.y;
+    perimeter += Math.abs(newPos.x - pos.x) + Math.abs(newPos.y - pos.y);
+    pos = newPos;
+  }
+
+  return (Math.abs(area) + perimeter) / 2 + 1;
 }
 
 const answer = await day18b();
