ce4ae52854142fd7651af77ca7b5bc64d44eb7a0
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-20 20:03:45 +0000

    feat: 🎸 add day 19, part A and B
    Part 1 of today was really fun, I loved that one. Part 2... much less.
    It was very difficult, and while my original logic was very close to the
    right solution, I did have to look up someone else's code to get to the
    right answer. Even with someone else's code, I still had trouble getting
    my original code to the same answer. However, after sleeping on it for a
    while and the use of some clever github copilot chatting, I finally got
    the right answer with my own code. Thank you for reading and have a
    wonderful day!

--- puzzles/day-19/day-19-a.ts
+++ puzzles/day-19/day-19-a.ts
@@ -1,9 +1,119 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const REJECT = 'R';
+const ACCEPT = 'A';
+
+const GT = '>';
+const LT = '<';
+
+interface Condition {
+  identifier: string;
+  operator: typeof GT | typeof LT;
+  value: number;
+  result: string | typeof ACCEPT | typeof REJECT;
+}
+
+interface EndCondition {
+  destination: string | typeof ACCEPT | typeof REJECT;
+}
+
+interface Part {
+  x: number;
+  m: number;
+  a: number;
+  s: number;
+}
+
+const evaluateCondition = (part: Part, condition: Condition) => {
+  if (condition.operator === GT) {
+    return part[condition.identifier] > condition.value;
+  } else {
+    return part[condition.identifier] < condition.value;
+  }
+};
+
+const runWorkflow = (
+  part: Part,
+  workflowIdentifier: string,
+  workflowMap: Map<string, [...Condition[], EndCondition]>,
+) => {
+  if (workflowIdentifier === ACCEPT) return true;
+  if (workflowIdentifier === REJECT) return false;
+  const workflow = workflowMap.get(workflowIdentifier);
+  for (let condition of workflow) {
+    if ('destination' in condition) {
+      if (condition.destination === ACCEPT) return true;
+      if (condition.destination === REJECT) return false;
+      else return runWorkflow(part, condition.destination, workflowMap);
+    } else {
+      if (evaluateCondition(part, condition))
+        return runWorkflow(part, condition.result, workflowMap);
+    }
+  }
+};
+
 export async function day19a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const [workflowsBlock, partsBlock] = (await readData(dataPath, true)).split(
+    '\n\n',
+  );
+  const workflows = new Map<string, [...Condition[], EndCondition]>();
+
+  workflowsBlock.split('\n').forEach((workflow) => {
+    const [identifier, conditionsStr] = workflow.split('{');
+    const conditions = conditionsStr.replace('}', '').split(',');
+    const parsedConditions = conditions.map((condition) => {
+      let newCondition: Condition | EndCondition;
+      if (condition.includes(GT)) {
+        const [id, valueAndResult] = condition.split(GT);
+        const [value, result] = valueAndResult.split(':');
+        newCondition = {
+          identifier: id,
+          operator: GT,
+          value: Number(value),
+          result,
+        };
+      } else if (condition.includes(LT)) {
+        const [id, valueAndResult] = condition.split(LT);
+        const [value, result] = valueAndResult.split(':');
+        newCondition = {
+          identifier: id,
+          operator: LT,
+          value: Number(value),
+          result,
+        };
+      } else {
+        newCondition = { destination: condition };
+      }
+      return newCondition;
+    }) as [...Condition[], EndCondition];
+
+    workflows.set(identifier, parsedConditions);
+  });
+
+  const parts: Part[] = partsBlock.split('\n').map((part) => {
+    const [xPart, mPart, aPart, sPart] = part
+      .slice(1, part.length - 1)
+      .split(',');
+    return {
+      x: Number(xPart.split('=')[1]),
+      m: Number(mPart.split('=')[1]),
+      a: Number(aPart.split('=')[1]),
+      s: Number(sPart.split('=')[1]),
+    };
+  });
+
+  const acceptedParts = parts
+    .map((part) => {
+      if (runWorkflow(part, 'in', workflows)) return part;
+      return null;
+    })
+    .filter(Boolean);
+
+  return acceptedParts.reduce(
+    (prev, { x, m, a, s }) => prev + x + m + a + s,
+    0,
+  );
 }
 
 const answer = await day19a();
--- puzzles/day-19/day-19-b.ts
+++ puzzles/day-19/day-19-b.ts
@@ -1,9 +1,117 @@
-import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import { readData } from '../../shared.ts';
+
+const REJECT = 'R';
+const ACCEPT = 'A';
+
+const GT = '>';
+const LT = '<';
+
+interface Condition {
+  identifier: string;
+  operator: typeof GT | typeof LT;
+  value: number;
+  result: string | typeof ACCEPT | typeof REJECT;
+}
+
+interface Part {
+  x: { start: number; end: number };
+  m: { start: number; end: number };
+  a: { start: number; end: number };
+  s: { start: number; end: number };
+}
+
+const runWorkflow = (
+  part: Part,
+  workflowIdentifier: string,
+  workflowMap: Map<string, Condition[]>,
+) => {
+  if (workflowIdentifier === REJECT) return 0;
+  if (workflowIdentifier === ACCEPT) {
+    return Object.values(part).reduce((product, { start, end }) => {
+      return product * (end - start + 1);
+    }, 1);
+  }
+
+  let combos = 0;
+  for (const { identifier, operator, value, result } of workflowMap.get(
+    workflowIdentifier,
+  )) {
+    const { start: low, end: high } = part[identifier];
+
+    if ((operator === GT && low > value) || (operator === LT && low < value)) {
+      if (
+        (operator === GT && high > value) ||
+        (operator === LT && high < value)
+      ) {
+        return combos + runWorkflow(part, result, workflowMap);
+      } else {
+        combos += runWorkflow(
+          {
+            ...part,
+            [identifier]: {
+              start: low,
+              end: value - 1,
+            },
+          },
+          result,
+          workflowMap,
+        );
+        part[identifier] = { start: value, end: high };
+      }
+    } else if (
+      (operator === GT && high > value) ||
+      (operator === LT && high < value)
+    ) {
+      combos += runWorkflow(
+        {
+          ...part,
+          [identifier]: { start: value + 1, end: high },
+        },
+        result,
+        workflowMap,
+      );
+      part[identifier] = { start: low, end: value };
+    }
+  }
+};
 
 export async function day19b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const [workflowsBlock] = (await readData(dataPath, true)).split('\n\n');
+  const workflows = new Map<string, Condition[]>();
+
+  workflowsBlock.split('\n').forEach((workflow) => {
+    const [identifier, conditionsStr] = workflow.split('{');
+    const conditions = conditionsStr.replace('}', '').split(',');
+    const parsedConditions = conditions.map((condition) => {
+      if (condition.includes(GT) || condition.includes(LT)) {
+        const operator = condition.includes(GT) ? GT : LT;
+        const [id, valueAndResult] = condition.split(operator);
+        const [value, result] = valueAndResult.split(':');
+        return {
+          identifier: id,
+          operator,
+          value: Number(value),
+          result,
+        } satisfies Condition;
+      } else {
+        return { identifier: 'x', operator: GT, value: 0, result: condition };
+      }
+    }) as Condition[];
+
+    workflows.set(identifier, parsedConditions);
+  });
+
+  return runWorkflow(
+    {
+      x: { start: 1, end: 4000 },
+      m: { start: 1, end: 4000 },
+      a: { start: 1, end: 4000 },
+      s: { start: 1, end: 4000 },
+    },
+    'in',
+    workflows,
+  );
 }
 
 const answer = await day19b();
