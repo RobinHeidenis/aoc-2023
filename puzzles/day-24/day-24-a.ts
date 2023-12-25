747ee90711752f5f29ade49212111fed69e7707b
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-25 14:43:16 +0100

    feat: 🎸 add day 24, part A and B
    This one was impossible for me. It was super math heavy, all of which I
    had never done or even seen before. I had a lot of trouble. For part 1 I
    could follow HyperNeutrino's video, but for part 2 they used a Python
    library, which we obviously don't have available in JavaScript.
    Therefore I had to look for a different implementation to follow, and
    ayoubzulfiqar came to the rescue with their implementation of the
    problem. I wish I could've solved this myself, but at least I understand
    on a high level what happens and what's going on in the code. Mentally I
    know on a high level how to solve the problem, but I'm not sure about
    the specifics. It's still Christmas today, so I hope you have an amazing
    day and MERRY CHRISTMAS 🎄🎄🎄

--- puzzles/day-24/day-24-a.ts
+++ puzzles/day-24/day-24-a.ts
@@ -1,9 +1,105 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+// class Hailstone:
+// def __init__(self, sx, sy, sz, vx, vy, vz):
+// self.sx = sx
+// self.sy = sy
+// self.sz = sz
+// self.vx = vx
+// self.vy = vy
+// self.vz = vz
+//
+// self.a = vy
+// self.b = -vx
+// self.c = vy * sx - vx * sy
+//
+// def __repr__(self):
+// return "Hailstone{" + f"a={self.a}, b={self.b}, c={self.c}" + "}"
+//
+// hailstones = [Hailstone(*map(int, line.replace("@", ",").split(","))) for line in open(0)]
+//
+// total = 0
+//
+// for i, hs1 in enumerate(hailstones):
+// for hs2 in hailstones[:i]:
+// a1, b1, c1 = hs1.a, hs1.b, hs1.c
+// a2, b2, c2 = hs2.a, hs2.b, hs2.c
+// if a1 * b2 == b1 * a2:
+// continue
+// x = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1)
+// y = (c2 * a1 - c1 * a2) / (a1 * b2 - a2 * b1)
+// if 200000000000000 <= x <= 400000000000000 and 200000000000000 <= y <= 400000000000000:
+// if all((x - hs.sx) * hs.vx >= 0 and (y - hs.sy) * hs.vy >= 0 for hs in (hs1, hs2)):
+// total += 1
+//
+// print(total)
+
+class Hailstone {
+  public a: number;
+  public b: number;
+  public c: number;
+  constructor(
+    public sx: number,
+    public sy: number,
+    public sz: number,
+    public vx: number,
+    public vy: number,
+    public vz: number,
+  ) {
+    this.a = vy;
+    this.b = -vx;
+    this.c = vy * sx - vx * sy;
+  }
+}
+
 export async function day24a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const hailstones = data.map(
+    (line) =>
+      new Hailstone(
+        ...(line.replace('@', ',').split(',').map(Number) as [
+          number,
+          number,
+          number,
+          number,
+          number,
+          number,
+        ]),
+      ),
+  );
+
+  let total = 0;
+
+  for (let i = 0; i < hailstones.length; i++) {
+    const { a: a1, b: b1, c: c1 } = hailstones[i];
+    for (let j = 0; j < i; j++) {
+      const { a: a2, b: b2, c: c2 } = hailstones[j];
+      if (a1 * b2 === b1 * a2) continue;
+      const x = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1);
+      const y = (c2 * a1 - c1 * a2) / (a1 * b2 - a2 * b1);
+
+      // Sample min-max;
+      // const min = 7;
+      // const max = 27;
+      // Actual min-max;
+      const min = 200_000_000_000_000;
+      const max = 400_000_000_000_000;
+
+      if (x >= min && x <= max && y >= min && y <= max) {
+        if (
+          [hailstones[i], hailstones[j]].every(
+            (hs) => (x - hs.sx) * hs.vx >= 0 && (y - hs.sy) * hs.vy >= 0,
+          )
+        ) {
+          total++;
+        }
+      }
+    }
+  }
+
+  return total;
 }
 
 const answer = await day24a();
--- puzzles/day-24/day-24-b.ts
+++ puzzles/day-24/day-24-b.ts
@@ -1,9 +1,107 @@
-import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import { readData } from '../../shared.js';
 
+interface Hailstone {
+  px: number;
+  py: number;
+  pz: number;
+  vx: number;
+  vy: number;
+  vz: number;
+}
 export async function day24b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const hailstones: Hailstone[] = [];
+
+  const velocitiesX: Record<number, number[]> = {};
+  const velocitiesY: Record<number, number[]> = {};
+  const velocitiesZ: Record<number, number[]> = {};
+
+  const getRockVelocity = (velocities: Record<number, number[]>): number => {
+    let possibleV: number[] = [];
+    for (let x = -1000; x <= 1000; x++) {
+      possibleV.push(x);
+    }
+
+    Object.keys(velocities).forEach((velocity) => {
+      const vel = parseInt(velocity, 10);
+      if (velocities[vel].length < 2) {
+        return;
+      }
+
+      let newPossibleV: number[] = [];
+      possibleV.forEach((possible) => {
+        if (
+          (velocities[vel][0] - velocities[vel][1]) % (possible - vel) ===
+          0
+        ) {
+          newPossibleV.push(possible);
+        }
+      });
+
+      possibleV = newPossibleV;
+    });
+
+    return possibleV[0];
+  };
+
+  async function coordinatesOfInitialPosition() {
+    for await (const line of data) {
+      const [px, py, pz, vx, vy, vz] = line
+        .replace('@', ',')
+        .split(',')
+        .map(Number);
+
+      if (!velocitiesX[vx]) velocitiesX[vx] = [px];
+      else velocitiesX[vx].push(px);
+
+      if (!velocitiesY[vy]) velocitiesY[vy] = [py];
+      else velocitiesY[vy].push(py);
+
+      if (!velocitiesZ[vz]) velocitiesZ[vz] = [pz];
+      else velocitiesZ[vz].push(pz);
+
+      hailstones.push({ px, py, pz, vx, vy, vz });
+    }
+
+    const rvx = getRockVelocity(velocitiesX);
+    const rvy = getRockVelocity(velocitiesY);
+    const rvz = getRockVelocity(velocitiesZ);
+
+    const results: Record<number, number> = {};
+    for (let i = 0; i < hailstones.length; i++) {
+      for (let j = i + 1; j < hailstones.length; j++) {
+        const stoneA = hailstones[i];
+        const stoneB = hailstones[j];
+
+        const ma = (stoneA.vy - rvy) / (stoneA.vx - rvx);
+        const mb = (stoneB.vy - rvy) / (stoneB.vx - rvx);
+
+        const ca = stoneA.py - ma * stoneA.px;
+        const cb = stoneB.py - mb * stoneB.px;
+
+        const rpx = Number.parseInt(((cb - ca) / (ma - mb)).toString(), 10);
+        const rpy = Number.parseInt((ma * rpx + ca).toString(), 10);
+
+        const time = Math.round((rpx - stoneA.px) / (stoneA.vx - rvx));
+        const rpz = stoneA.pz + (stoneA.vz - rvz) * time;
+
+        const result = rpx + rpy + rpz;
+        if (!results[result]) {
+          results[result] = 1;
+        } else {
+          results[result]++;
+        }
+      }
+    }
+
+    return Object.keys(results).sort(
+      (a, b) => results[Number(b)] - results[Number(a)],
+    )[0];
+  }
+
+  return await coordinatesOfInitialPosition();
 }
 
 const answer = await day24b();
