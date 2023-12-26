0765d689aff91b0b71eb02cc0ab840013243ee06
Robin Heidenis <robin.heidenis@gmail.com>
2023-12-26 01:50:45 +0100

    feat: 🎸 add day 25, part A and B
    Another tough one, I had to steal someone else's solution. It's been a
    really tough couple of days, with difficult challenges. Hopefully next
    year I'll be able to solve every challenge by myself. Anyway, I hope you
    have an amazing day, and had an amazing christmas if you celebrate, and
    a happy new year and all the good luck for the next year! 🎄🎄🎄🎄🎄

--- puzzles/day-25/day-25-a.ts
+++ puzzles/day-25/day-25-a.ts
@@ -1,9 +1,125 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const findGroups = (
+  vertexCount: number,
+  edges: [number, number][],
+  desiredCuts: number,
+) => {
+  for (let i = 0; i < edges.length; ++i) {
+    const idx = Math.floor(Math.random() * (i + 1));
+    [edges[i], edges[idx]] = [edges[idx], edges[i]];
+  }
+
+  const groupParents = [-1];
+  const vertexGroups = new Uint16Array(vertexCount);
+  const groupPromotions = [-1];
+
+  const findParent = (v: number) => {
+    // Find the parent of a group
+    if (vertexGroups[v] === 0) {
+      return -1;
+    }
+
+    let group = vertexGroups[v];
+    while (group !== groupParents[group]) {
+      group = groupParents[group];
+    }
+
+    return group;
+  };
+
+  const isUnion = (v1: number, v2: number) => {
+    if (!vertexGroups[v1] && !vertexGroups[v2]) {
+      // Union operation implementation for new groups
+      const group = groupParents.length;
+      groupParents.push(group);
+      groupPromotions.push(1);
+      vertexGroups[v1] = group;
+      vertexGroups[v2] = group;
+    } else if (!vertexGroups[v1]) {
+      // Union operation implementation when v1 is not in a group
+      const g = (vertexGroups[v2] = findParent(v2));
+      ++groupPromotions[g];
+      vertexGroups[v1] = g;
+    } else if (!vertexGroups[v2]) {
+      // Union operation implementation when v2 is not in a group
+      const g = (vertexGroups[v1] = findParent(v1));
+      ++groupPromotions[g];
+      vertexGroups[v2] = g;
+    } else {
+      // Union operation implementation for merging existing groups
+      let g1 = findParent(v1);
+      let g2 = findParent(v2);
+
+      if (g1 !== g2) {
+        if (groupPromotions[g1] > groupPromotions[g2]) {
+          [g2, g1] = [g1, g2];
+        }
+
+        groupPromotions[g2] += groupPromotions[g1] + 1;
+        groupParents[g1] = g2;
+        vertexGroups[v1] = g2;
+        vertexGroups[v2] = g2;
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  let edgeIdx = 0;
+  while (vertexCount > 2) {
+    const [v1, v2] = edges[edgeIdx++];
+
+    if (isUnion(v1, v2)) {
+      --vertexCount;
+    }
+  }
+
+  let removedEdges = 0;
+  for (const [v1, v2] of edges) {
+    if (
+      (vertexGroups[v1] = findParent(v1)) !==
+      (vertexGroups[v2] = findParent(v2))
+    ) {
+      ++removedEdges;
+    }
+  }
+
+  return removedEdges === desiredCuts ? vertexGroups : null;
+};
+
 export async function day25a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const edges: [number, number][] = [];
+  const vToId = new Map<string, number>();
+
+  for (const line of data) {
+    const [identifier, connectionsString] = line.split(': ');
+
+    if (!vToId.has(identifier)) {
+      vToId.set(identifier, vToId.size);
+    }
+
+    for (const connection of connectionsString.split(' ')) {
+      if (!vToId.has(connection)) {
+        vToId.set(connection, vToId.size);
+      }
+
+      edges.push([vToId.get(identifier), vToId.get(connection)]);
+    }
+  }
+
+  while (true) {
+    const groups = findGroups(vToId.size, edges, 3);
+
+    if (groups !== null) {
+      const group1Count = groups.filter((x) => x === groups[0]).length;
+      return group1Count * (vToId.size - group1Count);
+    }
+  }
 }
 
 const answer = await day25a();
--- puzzles/day-25/day-25-b.ts
+++ puzzles/day-25/day-25-b.ts
@@ -1,10 +1,28 @@
-import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
-export async function day25b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+export async function day25b() {
+  return 'PRESS THE BIG RED BUTTON!!!!!';
 }
 
 const answer = await day25b();
-console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+console.log(chalk.bgGreen('Your Answer:'), chalk.red(answer));
+console.log(
+  '🎄🎄🎄🎄🎄🎄 ',
+  chalk.bgGreen('M') +
+    chalk.bgGreen('E') +
+    chalk.bgRed('R') +
+    chalk.bgRed('R') +
+    chalk.bgGreen('Y') +
+    chalk.bgGreen(' ') +
+    chalk.bgRed('C') +
+    chalk.bgRed('H') +
+    chalk.bgGreen('R') +
+    chalk.bgGreen('I') +
+    chalk.bgRed('S') +
+    chalk.bgRed('T') +
+    chalk.bgGreen('M') +
+    chalk.bgGreen('A') +
+    chalk.bgRed('S') +
+    chalk.bgRed('!'),
+  '🎄🎄🎄🎄🎄🎄🎄',
+);
